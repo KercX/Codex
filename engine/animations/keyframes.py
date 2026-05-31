52a4bbb4a5bff3d063c23ccc53e61a3c18197d69
Brat1shkaKERCIX <sasaslobodan174@gmail.com>
2026-05-31 20:02:26 +0300

    Update keyframes.py

--- engine/animations/keyframes.py
+++ engine/animations/keyframes.py
@@ -2,5 +2,6 @@
 class Keyframe:
     def __init__(self, obj_name, frame, position=None):
         self.obj_name = obj_name
+        self.size = size
         self.frame = frame
         self.position = position
